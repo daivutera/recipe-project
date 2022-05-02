a6f43d16d2b4c5cb0b335f51bbe9b9a052093b29
Daiva Rajackiene <daiva.rajackiene@gmail.com>
2022-05-02 16:24:51 +0300

    pasitaisiau styled klaida

--- src/components/styles.js
+++ src/components/styles.js
@@ -1,34 +1,24 @@
 import styled from 'styled-components';
 
 export const Div = styled.div`
-  && {
-    color: red;
-  }
+  color: red;
 `;
 
 export const Img = styled.img`
-  && {
-    width: 100%;
-    height: 10rem;
-    object-fit: cover;
-  }
+  width: 100%;
+  height: 10rem;
+  object-fit: cover;
 `;
 export const Logo = styled.img`
-   {
-    height: 5rem;
-    margin-bottom: 2rem;
-    margin-left: 33rem;
-    margin-right: auto;
-  }
+  height: 5rem;
+  margin-bottom: 2rem;
+  margin-left: 33rem;
+  margin-right: auto;
 `;
 export const Flex = styled.div`
-   {
-    display: flex;
-    justify-content: flex-start;
-  }
+  display: flex;
+  justify-content: flex-start;
 `;
 export const Link = styled.a`
-   {
-    margin: 1rem;
-  }
+  margin: 1rem;
 `;

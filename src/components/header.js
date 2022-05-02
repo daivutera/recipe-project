d43d5e3bc9b1dfa6f95154d298585901aeb8314b
Daiva Rajackiene <daiva.rajackiene@gmail.com>
2022-05-02 15:43:15 +0300

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,19 @@
+import * as S from './styles';
+import { Button } from './Button';
+
+export const Header = () => {
+  return (
+    <S.Div>
+      <S.Logo src='/recipes.png' alt=''></S.Logo>
+      <S.Flex>
+        <S.Link href='www.google.lt'>Home</S.Link>
+        <S.Link href='www.google.lt'>Recipes</S.Link>
+        <Button>+ Submit Recipe</Button>
+      </S.Flex>
+
+      <div>
+        <S.Img src='/food.jpg' alt=''></S.Img>
+      </div>
+    </S.Div>
+  );
+};
---
+++ src/components/styles.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+export const Div = styled.div`
+  && {
+    color: red;
+  }
+`;
+
+export const Img = styled.img`
+  && {
+    width: 100%;
+    height: 10rem;
+    object-fit: cover;
+  }
+`;
+export const Logo = styled.img`
+  && {
+    height: 5rem;
+    margin-bottom: 2rem;
+    margin-left: 33rem;
+    margin-right: auto;
+  }
+`;
+export const Flex = styled.div`
+  && {
+    display: flex;
+    justify-content: flex-start;
+  }
+`;
+export const Link = styled.a`
+  && {
+    margin: 1rem;
+  }
+`;
